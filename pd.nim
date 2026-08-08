baedc0f4c1f9e62295ff7a65414cf0e18d687b8d
Claudiu <claudiu-meiu@proton.me>
2026-08-09 02:20:51 +0300

    Refactored the basic implementation from the first commit and added features:
    - Print permissions
    - Print date and time of the last write
    - More readable formatted output

--- pd.nim
+++ pd.nim
@@ -1,4 +1,4 @@
-import std/[parseopt, paths, dirs]
+import std/[parseopt, os, strutils, strformat, times]
 
 
 var
@@ -7,15 +7,46 @@ var
   showFiles = false
 
 
-iterator getDirectories(path: Path): Path =
-  for kind, p in walkDir(path):
-    if kind == pcDir:
-      yield p
+proc formatPermissions(path: string): string =
+  let permissions = getFileInfo(path, false).permissions
+  var user: seq[string] = @[]
+  var group: seq[string] = @[]
+  var others: seq[string] = @[]
 
-iterator getFiles(path: Path): Path =
-  for kind, p in walkDir(path):
+  for perm in permissions:
+    case perm
+    of fpUserExec:
+      user.add("x")
+    of fpGroupExec:
+      group.add("x")
+    of fpOthersExec:
+      others.add("x")
+    of fpUserRead:
+      user.add("r")
+    of fpGroupRead:
+      group.add("r")
+    of fpOthersRead:
+      others.add("r")
+    of fpUserWrite:
+      user.add("w")
+    of fpGroupWrite:
+      group.add("w")
+    of fpOthersWrite:
+      others.add("w")
+
+  return user.join("") & "-" & group.join("") & "-" & others.join("")
+
+
+
+iterator directories(): string =
+  for kind, dirPath in walkDir(currentPath):
+    if kind == pcDir:
+      yield dirPath
+
+iterator files(): string =
+  for kind, filePath in walkDir(currentPath):
     if kind == pcFile:
-      yield p
+      yield filePath
 
 
 for kind, key, val in getopt():
@@ -43,17 +74,31 @@ Options:
       echo "Unknown option: ", key
       quit(1)
   of cmdArgument:
-    currentPath = Path(key)
+    currentPath = key
 
 
 if showDirs:
-  for dir in getDirectories(currentPath):
-    echo dir
+  for dir in directories():
+    let extractedDirName = dir.extractFilename
+    let dateTime = local(getFileInfo(dir).lastWriteTime).format("MMM d HH:mm")
+
+    echo fmt"{formatPermissions(dir):<12} {getFileInfo(dir).size:<12} {dateTime:<14} {extractedDirName}"
 
 elif showFiles:
-  for file in getFiles(currentPath):
-    echo file
+  for file in files():
+    let extractedFileName = file.extractFilename
+    let dateTime = local(getFileInfo(file).lastWriteTime).format("MMM d HH:mm")
+
+    echo fmt"{formatPermissions(file):<12} {getFileInfo(file).size:<12} {dateTime:<14} {extractedFileName}"
 
 else:
   for kind, path in walkDir(currentPath):
-    echo path
+    let extractedPathName = path.extractFilename
+    let dateTime = local(getFileInfo(path, false).lastWriteTime).format("MMM d HH:mm")
+
+    if symlinkExists(path):
+      echo fmt"{formatPermissions(path):<12} {getFileInfo(path, false).size:<12} {dateTime:<14} {extractedPathName} -> {expandSymlink(path)}"
+    else:
+      echo fmt"{formatPermissions(path):<12} {getFileInfo(path).size:<12} {dateTime:<14} {extractedPathName}"
+
+
