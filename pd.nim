a114a889cfaa0675b87bca09f07e4005952b2b72
Claudiu <claudiu-meiu@proton.me>
2026-08-10 06:17:23 +0300

    Reorganized the code and extracted some repeating logic into helpers.

--- pd.nim
+++ pd.nim
@@ -3,40 +3,8 @@ import std/[parseopt, os, strformat, times, terminal]
 
 var
   currentPath = getCurrentDir()
-  showDirs = false
-  showFiles = false
-
-let dateTimeFormat = "MMM d HH:mm"
-
-
-proc pathInfo(path: string): FileInfo =
-  result = getFileInfo(path, false)
-
-proc formatPermissions(path: string): string =
-  let permissions = getFileInfo(path, false).permissions
-
-  template bit(flag: FilePermission, letter: char): char =
-    if flag in permissions: letter else: '-'
-
-  result = ""
-  result.add bit(fpUserRead, 'r')
-  result.add bit(fpUserWrite, 'w')
-  result.add bit(fpUserExec, 'x')
-  result.add bit(fpGroupRead, 'r')
-  result.add bit(fpGroupWrite, 'w')
-  result.add bit(fpGroupExec, 'x')
-  result.add bit(fpOthersRead, 'r')
-  result.add bit(fpOthersWrite, 'w')
-  result.add bit(fpOthersExec, 'x')
-
-
-iterator directories(): string =
-  for kind, dirPath in walkDir(currentPath):
-    if kind == pcDir: yield dirPath
-
-iterator files(): string =
-  for kind, filePath in walkDir(currentPath):
-    if kind == pcFile: yield filePath
+  showOnlyDirs = false
+  showOnlyFiles = false
 
 
 for kind, key, val in getopt():
@@ -55,44 +23,75 @@ Options:
   -h, --help    Show this help
 """
       quit(0)
-    of "d", "dirs": showDirs = true
-    of "f", "files": showFiles = true
+    of "d", "dirs": showOnlyDirs = true
+    of "f", "files": showOnlyFiles = true
     else:
       echo "Unknown option: ", key
       quit(1)
   of cmdArgument: currentPath = key
 
 
-if showDirs:
+iterator directories(): string =
+  for kind, dirPath in walkDir(currentPath):
+    if kind == pcDir: yield dirPath
+
+iterator files(): string =
+  for kind, filePath in walkDir(currentPath):
+    if kind == pcFile: yield filePath
+
+
+proc pathInfo(path: string): FileInfo =
+  result = getFileInfo(path, false)
+
+proc formatDateTime(path: string): string =
+  let dateTimeFormat = "MMM d HH:mm"
+  result = local(pathInfo(path).lastWriteTime).format(dateTimeFormat)
+
+proc formatPermissions(path: string): string =
+  let permissions = getFileInfo(path, false).permissions
+
+  template flag(flag: FilePermission, letter: char): char =
+    if flag in permissions: letter else: '-'
+
+  result = ""
+  result.add flag(fpUserRead, 'r')
+  result.add flag(fpUserWrite, 'w')
+  result.add flag(fpUserExec, 'x')
+  result.add flag(fpGroupRead, 'r')
+  result.add flag(fpGroupWrite, 'w')
+  result.add flag(fpGroupExec, 'x')
+  result.add flag(fpOthersRead, 'r')
+  result.add flag(fpOthersWrite, 'w')
+  result.add flag(fpOthersExec, 'x')
+
+
+if showOnlyDirs:
   for dir in directories():
     let
       extractedDirName = dir.extractFilename
-      dateTime = local(pathInfo(dir).lastWriteTime).format(dateTimeFormat)
       coloredExtractedDirName = ansiForegroundColorCode(fgBlue) &
         extractedDirName & ansiResetCode
-      coloredDirType = ansiForegroundColorCode(fgBlue) & "D" & ansiResetCode
+      coloredDirType = ansiForegroundColorCode(fgBlue) & "d" & ansiResetCode
 
-    echo fmt"{coloredDirType} {formatPermissions(dir)} {pathInfo(dir).size:>8} {dateTime:>16} {coloredExtractedDirName}"
+    echo fmt"{coloredDirType} {formatPermissions(dir)} {pathInfo(dir).size:>8} {formatDateTime(dir):>16} {coloredExtractedDirName}"
 
-elif showFiles:
+elif showOnlyFiles:
   for file in files():
     let
       extractedFileName = file.extractFilename
-      dateTime = local(pathInfo(file).lastWriteTime).format(dateTimeFormat)
       coloredExtractedFileName = ansiForegroundColorCode(fgWhite) &
           extractedFileName & ansiResetCode
-      coloredFileType = ansiForegroundColorCode(fgWhite) & "F" & ansiResetCode
+      coloredFileType = ansiForegroundColorCode(fgWhite) & "f" & ansiResetCode
 
-    echo fmt"{coloredFileType} {formatPermissions(file)} {pathInfo(file).size:>8} {dateTime:>16} {coloredExtractedFileName}"
+    echo fmt"{coloredFileType} {formatPermissions(file)} {pathInfo(file).size:>8} {formatDateTime(file):>16} {coloredExtractedFileName}"
 
 else:
   for kind, path in walkDir(currentPath):
     let
       extractedPathName = path.extractFilename
-      dateTime = local(pathInfo(path).lastWriteTime).format(dateTimeFormat)
       pathType =
-        if kind == pcDir: "D"
-        elif kind == pcFile: "F"
+        if kind == pcDir: "d"
+        elif kind == pcFile: "f"
         elif symlinkExists(path): "-"
         else: ""
       color =
@@ -103,9 +102,9 @@ else:
       coloredPathType = color & pathType & ansiResetCode
 
     if symlinkExists(path):
-      echo fmt"{coloredPathType} {formatPermissions(path)} {pathInfo(path).size:>8} {dateTime:>16} {coloredExtractedPathName} -> {expandSymlink(path)}"
+      echo fmt"{coloredPathType} {formatPermissions(path)} {pathInfo(path).size:>8} {formatDateTime(path):>16} {coloredExtractedPathName} -> {expandSymlink(path)}"
     else:
-      echo fmt"{coloredPathType} {formatPermissions(path)} {pathInfo(path).size:>8} {dateTime:>16} {coloredExtractedPathName}"
+      echo fmt"{coloredPathType} {formatPermissions(path)} {pathInfo(path).size:>8} {formatDateTime(path):>16} {coloredExtractedPathName}"
 
 
 
